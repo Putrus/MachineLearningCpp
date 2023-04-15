f8ca4199e48ac63f26bd6ee6cc0023fb57879c4b
Marcin Putra <marcin.putra.0@gmail.com>
2023-04-15 20:18:16 +0200

    Add xtensor and xrl libraries

--- DeepLearningFromScratch/Foundations/src/main.cpp
+++ DeepLearningFromScratch/Foundations/src/main.cpp
@@ -3,45 +3,17 @@
 #include <vector>
 #include <algorithm>
 
-template<typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
-{
-   os << '[';
-   for (auto it = v.begin(); it != v.end(); ++it)
-   {
-      if (it != v.begin())
-         os << ", ";
-
-      os << *it;
-   }
-   os << ']';
-   return os;
-}
-
-template <typename T>
-void square(std::vector<T>& v)
-{
-   for (auto& element : v)
-   {
-      element = std::pow(element, 2);
-   }
-}
-
-template <typename T>
-void leakyRelu(std::vector<T>& v)
-{
-   for (auto& element : v)
-   {
-      element = std::max(static_cast<T>(0.2 * element), element);
-   }
-}
+#include <xtensor/xarray.hpp>
+#include <xtensor/xio.hpp>
+#include <xtensor/xview.hpp>
 
 int main()
 {
-   std::vector<int> v = { 1, -25, 3, 4 };
-   //square(v);
-   std::cout << v << std::endl;
-   leakyRelu(v);
-   std::cout << v << std::endl;
+   xt::xarray<double> arr2{ 5.0, 6.0, 7.0 };
+
+   auto s = xt::eye<double>({ 3,  4 });
+   arr2 *= 5;
+   std::cout << arr2 << std::endl;
+   std::cout << s << std::endl;
    return 0;
 }
